f8a285e9d70a84f510b39be31765469c53bfbe4f
Kingdavid-1234 <129787677+Kingdavid-1234@users.noreply.github.com>
2023-09-01 20:51:28 -0500

    Add files via upload

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author David Ndubuisi
  */
 
 public class Class1 {
@@ -8,5 +8,12 @@ public class Class1 {
     private void f1() {
         // v1.0
     }
+    private void f2() {
+        // git commit -m "Added f2() method"
 
+
+    }
+    private void f3() {
+        // Add code for f3() here
+    }
 }
